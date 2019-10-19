10a5b67a465d6d7c6f046e247fd980f9cc4b8dc0
NAlvarez <ruyl@users.noreply.github.com>
2019-10-19 02:19:57 +0000

    Change to work for new function
    Change positives to negatives and vice versa. Change names of functions in function calls.

--- test/test.js
+++ test/test.js
@@ -1,79 +1,79 @@
 var Code = require('code')
 var Lab = require('lab')
 
-var isPositiveInteger = require('../index.js')
-var isSafePositiveInteger = isPositiveInteger.isSafePositiveInteger
+var isNegativeInteger = require('../index.js')
+var isSafeNegativeInteger = isNegativeInteger.isSafeNegativeInteger
 
-var MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER || 9007199254740991
+var MIN_SAFE_INTEGER = Number.MIN_SAFE_INTEGER || -9007199254740991
 
 var lab = exports.lab = Lab.script()
 var describe = lab.describe
 var it = lab.it
 var expect = Code.expect
 
-describe('is-positive-integer', function () {
-  it('should return true for positive integers', function (done) {
-    expect(isPositiveInteger(1)).to.equal(true)
-    expect(isPositiveInteger(10)).to.equal(true)
-    expect(isPositiveInteger(100)).to.equal(true)
-    expect(isPositiveInteger(1000)).to.equal(true)
-    expect(isPositiveInteger(90071992555)).to.equal(true)
-    expect(isPositiveInteger(999999999999)).to.equal(true)
-    expect(isPositiveInteger(MAX_SAFE_INTEGER)).to.equal(true)
-    expect(isPositiveInteger(Number.MAX_VALUE)).to.equal(true)
-    expect(isPositiveInteger(new Number(12))).to.equal(true)
+describe('is-negative-integer', function () {
+  it('should return true for negative integers', function (done) {
+    expect(isNegativeInteger(-1)).to.equal(true)
+    expect(isNegativeInteger(-10)).to.equal(true)
+    expect(isNegativeInteger(-100)).to.equal(true)
+    expect(isNegativeInteger(-1000)).to.equal(true)
+    expect(isNegativeInteger(-90071992555)).to.equal(true)
+    expect(isNegativeInteger(-999999999999)).to.equal(true)
+    expect(isNegativeInteger(MIN_SAFE_INTEGER)).to.equal(true)
+    expect(isNegativeInteger(Number.MIN_VALUE)).to.equal(true)
+    expect(isNegativeInteger(new Number(-12))).to.equal(true)
     done()
   })
 
-  it('should return false for negative integers or zero', function (done) {
-    expect(isPositiveInteger(0)).to.equal(false)
-    expect(isPositiveInteger(-1)).to.equal(false)
-    expect(isPositiveInteger(-10)).to.equal(false)
-    expect(isPositiveInteger(-100)).to.equal(false)
-    expect(isPositiveInteger(-1000)).to.equal(false)
-    expect(isPositiveInteger(new Number(0))).to.equal(false)
-    expect(isPositiveInteger(new Number(-12))).to.equal(false)
+  it('should return false for positive integers or zero', function (done) {
+    expect(isNegativeInteger(0)).to.equal(false)
+    expect(isNegativeInteger(1)).to.equal(false)
+    expect(isNegativeInteger(10)).to.equal(false)
+    expect(isNegativeInteger(100)).to.equal(false)
+    expect(isNegativeInteger(1000)).to.equal(false)
+    expect(isNegativeInteger(new Number(0))).to.equal(false)
+    expect(isNegativeInteger(new Number(12))).to.equal(false)
     done()
   })
 
   it('should return false for floats', function (done) {
-    expect(isPositiveInteger(1.1)).to.equal(false)
-    expect(isPositiveInteger(10.1)).to.equal(false)
-    expect(isPositiveInteger(100.1)).to.equal(false)
-    expect(isPositiveInteger(1000.1)).to.equal(false)
-    expect(isPositiveInteger(-1.1)).to.equal(false)
-    expect(isPositiveInteger(-10.1)).to.equal(false)
-    expect(isPositiveInteger(-100.1)).to.equal(false)
-    expect(isPositiveInteger(-1000.1)).to.equal(false)
-    expect(isPositiveInteger(new Number(12.45))).to.equal(false)
+    expect(isNegativeInteger(-1.1)).to.equal(false)
+    expect(isNegativeInteger(-10.1)).to.equal(false)
+    expect(isNegativeInteger(-100.1)).to.equal(false)
+    expect(isNegativeInteger(-1000.1)).to.equal(false)
+    expect(isNegativeInteger(1.1)).to.equal(false)
+    expect(isNegativeInteger(10.1)).to.equal(false)
+    expect(isNegativeInteger(100.1)).to.equal(false)
+    expect(isNegativeInteger(1000.1)).to.equal(false)
+    expect(isNegativeInteger(new Number(-12.45))).to.equal(false)
     done()
   })
 
   it('should return false for others', function (done) {
-    expect(isPositiveInteger(Infinity)).to.equal(false)
-    expect(isPositiveInteger({})).to.equal(false)
-    expect(isPositiveInteger([])).to.equal(false)
-    expect(isPositiveInteger('10')).to.equal(false)
-    expect(isPositiveInteger('what')).to.equal(false)
-    expect(isPositiveInteger(/what/)).to.equal(false)
-    expect(isPositiveInteger(null)).to.equal(false)
-    expect(isPositiveInteger(undefined)).to.equal(false)
-    expect(isPositiveInteger(true)).to.equal(false)
-    expect(isPositiveInteger({ valueOf: function () { return 42 } })).to.equal(false)
-    expect(isPositiveInteger(Number.NaN)).to.equal(false)
+    expect(isNegativeInteger(Infinity)).to.equal(false)
+    expect(isNegativeInteger({})).to.equal(false)
+    expect(isNegativeInteger([])).to.equal(false)
+    expect(isNegativeInteger('-10')).to.equal(false)
+    expect(isNegativeInteger('what')).to.equal(false)
+    expect(isNegativeInteger(/what/)).to.equal(false)
+    expect(isNegativeInteger(null)).to.equal(false)
+    expect(isNegativeInteger(undefined)).to.equal(false)
+    expect(isNegativeInteger(true)).to.equal(false)
+    expect(isNegativeInteger({ valueOf: function () { return -42 } })).to.equal(false)
+    expect(isNegativeInteger(Number.NaN)).to.equal(false)
     done()
   })
 
-  describe('isSafePositiveInteger', function() {
-    it('should return true for positive integers', function (done) {
-      expect(isSafePositiveInteger(1)).to.equal(true)
-      expect(isSafePositiveInteger(10)).to.equal(true)
-      expect(isSafePositiveInteger(100)).to.equal(true)
-      expect(isSafePositiveInteger(1000)).to.equal(true)
-      expect(isSafePositiveInteger(90071992555)).to.equal(true)
-      expect(isSafePositiveInteger(999999999999)).to.equal(true)
-      expect(isSafePositiveInteger(MAX_SAFE_INTEGER)).to.equal(true)
-      expect(isSafePositiveInteger(Number.MAX_VALUE)).to.equal(false)
+  describe('isSafeNegativeInteger', function() {
+    it('should return true for negative integers', function (done) {
+      expect(isSafeNegativeInteger(-1)).to.equal(true)
+      expect(isSafeNegativeInteger(-10)).to.equal(true)
+      expect(isSafeNegativeInteger(-100)).to.equal(true)
+      expect(isSafeNegativeInteger(-1000)).to.equal(true)
+      expect(isSafeNegativeInteger(-90071992555)).to.equal(true)
+      expect(isSafeNegativeInteger(-999999999999)).to.equal(true)
+      expect(isSafeNegativeInteger(MIN_SAFE_INTEGER)).to.equal(true)
+      expect(isSafeNegativeInteger(Number.MIN_VALUE)).to.equal(false)
       done()
     })
   })

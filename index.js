f1a259c2ada3c3a1e56fcab12b7f653e1ea96ca0
NAlvarez <ruyl@users.noreply.github.com>
2019-10-19 01:59:02 +0000

    Swap posititive to negative
    Change logic to check for being negative and to compare against the MIN_SAFE_INTEGER instead of checking for being positive and checking against the MAX_SAFE_INTEGER. Also change function names.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 /* $lab:coverage:off$ */
-var MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER || 9007199254740991
+var MIN_SAFE_INTEGER = Number.MIN_SAFE_INTEGER || -9007199254740991
 /* $lab:coverage:on$ */
 
-module.exports = isPositiveInteger
-module.exports.isSafePositiveInteger = isSafePositiveInteger
+module.exports = isNegativeInteger
+module.exports.isSafeNegativeInteger = isSafeNegativeInteger
 
-function isPositiveInteger (x) {
+function isNegativeInteger (x) {
   // Is it a number?
   return Object.prototype.toString.call(x) === '[object Number]' &&
     // Is it an integer?
     x % 1 === 0 &&
-    // Is it positive?
-    x > 0
+    // Is it negative?
+    x < 0
 }
 
-// strict positive integer check:
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MAX_SAFE_INTEGER
-function isSafePositiveInteger (x) {
-  return isPositiveInteger(x) &&
-    x <= MAX_SAFE_INTEGER
+// strict negative integer check:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/MIN_SAFE_INTEGER
+function isSafeNegativeInteger (x) {
+  return isNegativeInteger(x) &&
+    x >= MIN_SAFE_INTEGER
 }
